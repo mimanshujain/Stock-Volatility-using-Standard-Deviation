12bd7ec7f92a8c070819f9f6cf598a23aa979230
mimanshujain <mimanshujain@gmail.com>
2015-02-25 23:26:47 -0500

    Final Changes

--- DicProj1Test/src/Reducer/StockReducer1.java
+++ DicProj1Test/src/Reducer/StockReducer1.java
@@ -1,84 +1,193 @@
 package Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.TreeMap;
 
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class StockReducer1 extends Reducer<Text, Text, Text, Text>{
 
-//	int firstDay = 0, lastDay = 0;
+	int firstDay = 0, lastDay = 0;
 	String firstPrice = "";
 	String lastPrice = "";
-//	firstDay == 0 && lastDay == 0 && 
-	public void reduce(Text key, Iterable<Text> values,Context context) throws IOException, InterruptedException {			
-		if(lastPrice.equals("") && firstPrice.equals(""))
+	private static Text countText= new Text("-1");
+	private static int count = 0;
+	//	firstDay == 0 && lastDay == 0 && 
+
+	public void reduce(Text key, Iterable<Text> values,Context context) throws IOException, InterruptedException {		
+
+		HashMap<String, TreeMap<String, String>> map = new HashMap<String, TreeMap<String,String>>();
+
+		Iterator<Text> it = values.iterator();
+		String[] data;
+		String name = "";
+		String day = "";
+		String adjClose = "";
+		ArrayList<Double> diffList = new ArrayList<Double>();
+		//		ArrayList<Double> valueList = new ArrayList<Double>();
+		TreeMap<String, String> tree = null;
+		while(it.hasNext())
 		{
-			Iterator<Text> it = values.iterator();
-			String lastData =  "", firstData = "";
-			Text trans = null;
-			
-			while(it.hasNext())
+			data = it.next().toString().split(" ");
+			name = key.toString()+" "+data[2]+" "+data[3];
+			day = data[0];
+			adjClose = data[1];
+			if(map.containsKey(name))
 			{
-				if(lastData.equals(""))
-					lastData = it.next().toString();
-				else
-					trans = it.next();
+				tree = map.get(name);
+				tree.put(day, adjClose);
 			}
-			
-			firstData = trans.toString();
-			lastPrice = lastData.split(" ")[1];
-			firstPrice = firstData.split(" ")[1];
+			else
+			{
+				tree = new TreeMap<String, String>();
+				tree.put(day, adjClose);
+				map.put(name, tree);
+			}
+			tree = null;
+		}
+		tree = null;
+		Iterator<String> it2 = map.keySet().iterator();
+		double first= 0D;
+		double last = 0D;
+		double diff = 0D;
+		while(it2.hasNext())
+		{
+			tree = map.get(it2.next());
+			firstPrice = tree.get(tree.firstKey());
+			lastPrice = tree.get(tree.lastKey());
 
-//			for(Text value : values)
-//			{
-//				String[] datePrice = value.toString().split(" ");
-//				int day = Integer.parseInt(datePrice[0]);
-//				String price = datePrice[1];
-//				setRange(day, price);
-//			}
-			double first = Double.parseDouble(firstPrice);
-			double last = Double.parseDouble(lastPrice);
-			Double diff = (last - first)/first;
-			DoubleWritable priceDiff = new DoubleWritable(diff);
-			Text dummy = new Text(priceDiff+"");
-			//		reduceVal.set(priceDiff.toString());
-			context.write(key, dummy);		
+			first = Double.parseDouble(firstPrice);
+			last = Double.parseDouble(lastPrice);
+			diff = (last - first)/first;
+			diffList.add(diff);
+			tree = null;
+		}
+		double sum = 0D;
+		int counter = 0;
+		Iterator<Double> it3 = diffList.iterator();
+		while(it3.hasNext())
+		{
+			sum = sum + it3.next();
+			counter++;
+		}
+		sum = sum/counter;
 
-//			firstDay = 0;
-//			lastDay = 0;
+		it3 = diffList.iterator();
+		double addDiff = 0D;
+		while(it3.hasNext())
+		{
+			diff = 0D;
+			diff = it3.next() - sum;
+			diff = Math.pow(diff, 2.0);
+			addDiff = addDiff + diff;
+			//			valueList.add();
+		}
+		//		double addDiff = 0D;
+		//		it3 = valueList.iterator();
+		//		while(it3.hasNext())
+		//			addDiff = addDiff + it3.next();
+
+		if(counter > 1)
+			addDiff = addDiff/(counter - 1);
+		if(addDiff != 0.0)
+		{
+			//		DoubleWritable volatility = new DoubleWritable();
+			Text dummy = new Text(Math.sqrt(addDiff)+""); //+" "+sum+" "+addDiff+" "+counter+" "+c	Math.sqrt(addDiff)
+			context.write(dummy, key);
 			firstPrice = "";
 			lastPrice = "";
+//			StockReducer1.count++;
 		}
 	}
-
-//	private void setRange(int day, String price)
-//	{
-//		if(firstDay == 0 && lastDay == 0)
-//		{
-//			firstDay = day;
-//			lastDay = day;
-//			firstPrice = price;
-//			lastPrice = price;
-//		}
 //
-//		else
-//		{
-//			if(day < firstDay)
-//			{
-//				firstDay = day;
-//				firstPrice = price;
-//				return;
-//			}
-//			if(day > lastDay)
-//			{
-//				lastDay = day;
-//				lastPrice = price;
-//				return;
-//			}
-//		}
-//	}	
+//	@Override
+//	protected void cleanup(Context context)
+//			throws IOException, InterruptedException {
+//		 
+//		context.write(countText, new Text(StockReducer1.count+"-" + new Date().getTime()));
+//
+//	}
+
+	//	public void reduce(Text key, Iterable<Text> values,Context context) throws IOException, InterruptedException {			
+	//		
+	////		Iterator<Text> it = values.iterator();
+	////		while(it.hasNext())
+	////		{
+	////			context.write(key, it.next());
+	////		}
+	//		
+	//		if(lastPrice.equals("") && firstPrice.equals(""))
+	//		{
+	//			Iterator<Text> it = values.iterator();
+	////			String lastData =  "", firstData = "";
+	////			Text trans = null;
+	//			String[] data;
+	//			while(it.hasNext())
+	//			{
+	////				if(firstData.equals(""))
+	////					firstData = it.next().toString();
+	////				else
+	////					trans = it.next();
+	//				data = it.next().toString().split(" ");
+	//				setRange(Integer.parseInt(data[0]), data[1]);
+	//			}
+	//			
+	////			lastData = trans.toString();
+	////			lastPrice = lastData.split(" ")[1];
+	////			firstPrice = firstData.split(" ")[1];
+	//
+	////			for(Text value : values)
+	////			{
+	////				String[] datePrice = value.toString().split(" ");
+	////				int day = Integer.parseInt(datePrice[0]);
+	////				String price = datePrice[1];
+	////				setRange(day, price);
+	////			}
+	//			double first = Double.parseDouble(firstPrice);
+	//			double last = Double.parseDouble(lastPrice);
+	//			Double diff = (last - first)/first;
+	//			DoubleWritable priceDiff = new DoubleWritable(diff);
+	//			Text dummy = new Text(priceDiff+" " + firstPrice+" "+lastPrice + " " + diff);
+	//			//		reduceVal.set(priceDiff.toString());
+	//			context.write(key, dummy);		
+	//
+	//			firstDay = 0;
+	//			lastDay = 0;
+	//			firstPrice = "";
+	//			lastPrice = "";
+	//		}
+	//	}
+
+	private void setRange(int day, String price)
+	{
+		if(firstDay == 0 && lastDay == 0)
+		{
+			firstDay = day;
+			lastDay = day;
+			firstPrice = price;
+			lastPrice = price;
+		}
+
+		else
+		{
+			if(day < firstDay)
+			{
+				firstDay = day;
+				firstPrice = price;
+				return;
+			}
+			if(day > lastDay)
+			{
+				lastDay = day;
+				lastPrice = price;
+				return;
+			}
+		}
+	}	
 
 }
--- DicProj1Test/src/Reducer/StockReducer3.java
+++ DicProj1Test/src/Reducer/StockReducer3.java
@@ -1,19 +1,87 @@
 package Reducer;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+
 public class StockReducer3 extends Reducer<DoubleWritable,Text , Text, Text>{
+
+	static int count = 0;
+	static int counter = 0;
+	static long time;
+	static HashMap<String, Double> map = new HashMap<String, Double>();
+	static String keyVal = "";
 	
 	public void reduce(DoubleWritable key, Iterable<Text> values,Context context) throws IOException, InterruptedException {	
-		
+
 		for(Text value : values)
 		{
-			context.write(value, new Text(key+""));
-		}
+			if(StockReducer3.counter == 0)
+				context.write(new Text("\nThe top 10 stocks with the lowest (min) volatility are"), new Text(""));
+			
+			if(StockReducer3.counter < 10)
+				keyVal = keyVal + "\n" + value.toString() + "     " +  key.get();
+			else if(StockReducer3.counter == 10)
+			{
+				context.write(new Text(keyVal), new Text());
+				StockReducer3.map.put(value.toString(), key.get());
+			}
+			
+			else if(StockReducer3.counter > 10)
+				StockReducer3.map.put(value.toString(), key.get());
+				
+			StockReducer3.counter++;
+		}		
 	}
 
+	@Override
+	protected void cleanup(Context context)
+			throws IOException, InterruptedException {
+		
+		ValueComparator val = new ValueComparator(StockReducer3.map);
+		TreeMap<String, Double> tree = new TreeMap<String, Double>(val);
+		tree.putAll(StockReducer3.map);
+		int c = 0;
+		Iterator<String> it = tree.keySet().iterator();
+		context.write(new Text("\nThe top 10 stocks with the highest (max) volatility are"), new Text(""));
+		String str = "";
+		while(it.hasNext())
+		{
+			String key = it.next();
+			str = str + "\n" + key + "     " +  map.get(key);
+			c++;
+			if(c > 9)
+				break;
+		}
+		context.write(new Text(str), new Text());
+	}
 }
+
+class ValueComparator implements Comparator<String>
+{
+	HashMap<String, Double> map;
+	
+	public ValueComparator(HashMap<String, Double> map) {
+		this.map = map;
+	}
+	
+	@Override
+	public int compare(String o1, String o2) {
+		
+		if(map.get(o1) >= map.get(o2))
+		{
+			return -1;
+		}
+		
+		else return 1;
+	}
+	
+}
+//context.write(value, new Text(key+""));
